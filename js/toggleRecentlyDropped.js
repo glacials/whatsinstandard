2bdda6a6a2940f256b741b4e3e9c6415690fe247
glacials <qhiiyr@gmail.com>
2016-07-05 22:31:26 -0500

    Change recently-dropped text when toggling
    Change "show recently dropped sets" to "hide recently dropped sets" when they're being shown. Closes #33.

--- js/toggleRecentlyDropped.js
+++ js/toggleRecentlyDropped.js
@@ -1,10 +1,17 @@
+var showText = "show recently dropped sets";
+var hideText = "hide recently dropped sets";
+
+var toggler = $("#recently-dropped-toggler")[0];
+var sets = $("#recently-dropped")[0];
+
 var toggleRecentlyDropped = function() {
-  var el = $("#recently-dropped")[0];
-  if(el.style.display == 'none') {
-    el.style.display = 'block';
+  if(sets.style.display == 'none') {
+    sets.style.display = 'block';
+    toggler.innerHTML = hideText;
     ga('send', 'event', 'link', 'click', 'show recently dropped sets');
   } else {
-    el.style.display = 'none';
+    sets.style.display = 'none';
+    toggler.innerHTML = showText;
     ga('send', 'event', 'link', 'click', 'hide recently dropped sets');
   }
 };

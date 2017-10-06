77fea6cf58366740475e7a27bb7e654fbca1e763
glacials <qhiiyr@gmail.com>
2017-10-05 23:42:14 -0700

    More functional :)
    Also fix the tooltips that were still broken :D

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var code = Vue.component('set-image', {
       <i class="ss" :class="imsym"></i>
     </span>
   `,
-  data: function () {
+  data: function() {
     return {
       imsym: 'ss-' + this.code.toLowerCase()
     }
@@ -14,68 +14,52 @@ var code = Vue.component('set-image', {
 })
 
 var app = new Vue({
-  el: '#vue-container',
+  el: '#vue',
 
   data: {
-    sets: [],
+    sets: []
   },
 
-  created: function () {
-    this.fetchData()
+  created: function() {
+    let self = this;
+
+    fetch(apiURL).then(function(response) {
+      response.json().then(function(data) {
+        self.sets = data.sets;
+      });
+    });
   },
 
   filters: {
-    moment: function (date) {
+    moment: function(date) {
       return moment(date).format('MMMM Do, YYYY');
     }
   },
 
   methods: {
-    fetchData: function () {
-      let self = this;
-
-      let setsByBlock = {};
-      let recentlyDroppedSetsByBlock = {};
-
-      let xhr = new XMLHttpRequest()
-      xhr.open('GET', apiURL);
-      xhr.onload = function () {
-        self.sets = JSON.parse(xhr.responseText).sets;
-      };
-      xhr.send();
-    },
-
     dropped: function(sets) {
       return sets.filter(function(set) {
-        return Date.parse(set.exit_date) < Date.now();
-      });
-    },
-
-    standard: function(sets) {
-      return sets.filter(function(set) {
-        return ((Date.parse(set.enter_date) || Infinity) <= Date.now())
-            && ((Date.parse(set.exit_date ) || Infinity) >= Date.now());
+        return (Date.parse(set.exit_date) || Infinity) <= Date.now();
       });
     },
 
     unreleased: function(sets) {
       return sets.filter(function(set) {
-        return Date.parse(set.enter_date) > Date.now();
+        return (Date.parse(set.enter_date) || Infinity) > Date.now();
       });
+    },
+
+    standard: function(sets) {
+      return _.difference(sets, this.unreleased(sets), this.dropped(sets));
     },
 
     blocks: function(sets) {
-      let blocks = {};
-      sets.forEach(function(set) {
-        if(!blocks[set.block]) {
-          blocks[set.block] = [];
-        }
-        blocks[set.block].push(set);
-      });
-      return blocks;
+      return sets.reduce(function(blocks, set) {
+        return _.extend(blocks, {[set.block]: (blocks[set.block] || []).concat(set)});
+      }, {});
     },
 
-    isReleased: function (set) {
+    isReleased: function(set) {
       return Date.parse(set.enter_date) <= Date.now();
     },
 

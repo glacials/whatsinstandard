aac06b51e855e927d1461e1b330528cc912bfac1
glacials <qhiiyr@gmail.com>
2020-06-04 11:47:25 -0700

    Debug deploys

--- .yourbase_deploy.sh
+++ .yourbase_deploy.sh
@@ -17,13 +17,10 @@ mkdir -p ${BUILD_DIR}
 # Replace these lines with any steps necessary to get your application ready for
 # deploy.
 npm install
-npm build
 
-# You shouldn't need to change anything below here.
 cd ${BUILD_DIR}
-git init
-git remote add origin https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}
+git remote add pages https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}
 git checkout -b gh-pages
 git add .
 git -c "user.name=${COMMITTER_NAME}" -c "user.email=${COMMITTER_EMAIL}" commit -am "${COMMIT_MESSAGE}"
-git push origin gh-pages --force
+git push pages gh-pages --force

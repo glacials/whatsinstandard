5fa386ea4c2b02a28cccb9f514f7b1aeece204e0
Ben Carlsson <qhiiyr@gmail.com>
2020-09-24 16:24:48 -0700

    Fix a remote bug in yourbase_deploy

--- .yourbase_deploy.sh
+++ .yourbase_deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-GITHUB_USERNAME="glacials"      # Must be the same user $GITHUB_TOKEN belongs to
-GITHUB_ORGANIZATION="glacials"  # Organization that holds the repository
-GITHUB_REPOSITORY="whatsinstandard"    # Repository name (e.g. test-js-app)
-BUILD_DIR="."                # Set this to where your application gets built into
+GITHUB_USERNAME="glacials"          # The user $GITHUB_TOKEN belongs to
+GITHUB_ORGANIZATION="glacials"      # Organization that holds the repository
+GITHUB_REPOSITORY="whatsinstandard" # Repository name (e.g. test-js-app)
+BUILD_DIR="."                       # Where the application gets built into
 
 # This is the author and message information YourBase will use on each deploy
 # commit. These commits will only appear on gh-pages, not on master, and will
@@ -19,8 +19,12 @@ mkdir -p ${BUILD_DIR}
 npm install
 
 cd ${BUILD_DIR}
-git remote add pages https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}
+
+# Create the "pages" remote if it doesn't exist. This might be the same
+# destination as origin, but we're going to use our credentials for this one.
+git config remote.pages.url >&- || git remote add pages https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}
+
 git checkout -b gh-pages
 git add .
 git -c "user.name=${COMMITTER_NAME}" -c "user.email=${COMMITTER_EMAIL}" commit --allow-empty -am "${COMMIT_MESSAGE}"
-git push pages gh-pages --force
+git push --force pages gh-pages

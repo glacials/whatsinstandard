a5dd95a3eb03fe4b0b7ff9a04826316c3ad2e3d6
glacials <ben@twos.dev>
2023-01-31 23:38:47 -0800

    Add Mastodon tooting to the autotweeter

--- firebase/README.md
+++ firebase/README.md
@@ -1,22 +1,26 @@
 # Firebase
 
-This directory holds the Firebase Function `maybeTweet`, which checks periodically against the _What's in Standard?_ API for changes to Standard, then tweets those changes from [@whatsinstandard][@whatsinstandard].
+This directory holds the Firebase Function `maybeTweet`, which checks periodically against the _What's in Standard?_ API for changes to Standard, then tweets and toots those changes from [@whatsinstandard][@whatsinstandard].
 
 ## Development
 
 To test locally, you need `firebase-tools `installed.
+
 ```sh
 firebase emulators:start
 ```
 
 Hit the function from a different terminal:
+
 ```sh
-curl -i http://127.0.0.1:5001/whats-in-standard/us-central1/maybeTweet
+curl -i http://127.0.0.1:5001/whats-in-standard/us-central1/maybeTweet -d '{}'
 ```
 
-To actually test tweeting, you will need some config variables set:
+To actually test tweeting, you will need some config variables set. **WARNING: Depending on your Firebase auth, you may have access to production secrets without realizing it. However you will be using a development (read: empty) Firestore database, so testing this will actually send a tweet when the function thinks it hasn't tweeted a past change yet.**
+
 ```sh
 firebase functions:config:set twitter.access_token=CHANGEME twitter.access_token_secret=CHANGEME twitter.api_key=CHANGEME twitter.api_secret=CHANGEME
+firebase functions:config:set mastodon.server.url=CHANGEME mastodon.access_token=CHANGEME
 firebase functions:config:get > functions/.runtimeconfig.json
 ```
 
@@ -24,11 +28,13 @@ To see the database state, you can use the Firebase emualator UI. It should
 boot with the emulators, and give you a link to access it.
 
 ### Architecture
+
 The cron architecture is embedded in the Firebase function; there is no
 separate architecture that pings it. Look for the `functions.pubsub.schedule`
 call.
 
 ### Deploying
+
 ```sh
 firebase deploy
 ```
--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,92 +1,167 @@
-const functions = require("firebase-functions")
-const fetch = require("node-fetch")
-const assert = require('assert')
+const functions = require("firebase-functions");
+const fetch = require("node-fetch");
+const assert = require("assert");
 
-const twitter = require('twitter-api-client')
+const mastodon = require("masto");
+const twitter = require("twitter-api-client");
 
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
 
-const admin = require("firebase-admin")
-admin.initializeApp()
-const db = admin.firestore()
+const MAX_TOOT_LENGTH = 500;
+const MAX_TWEET_LENGTH = 280;
 
-collection = db.collection('twitterbot/last-known/sets')
+collection = db.collection("twitterbot/last-known/sets");
 
-exports.maybeTweet = functions.pubsub.schedule('every 5 minutes').onRun(async (context) => {
-  setsByName = {}
+exports.maybeTweet = functions.pubsub
+  .schedule("every 5 minutes")
+  .onRun(async (context) => {
+    setsByName = {};
 
-  wisSets = await standardSets()
-  wisSets.forEach(set => setsByName[set.name] = set)
-  wisSetNames = new Set(wisSets.map(set => set.name))
+    wisSets = await standardSets();
+    wisSets.forEach((set) => (setsByName[set.name] = set));
+    wisSetNames = new Set(wisSets.map((set) => set.name));
 
-  cacheSets = await collection.get()
-  cacheSetNames = new Set()
-  cacheSets.forEach(doc => {
-    const set = doc.data()
+    cacheSets = await collection.get();
+    cacheSetNames = new Set();
+    cacheSets.forEach((doc) => {
+      const set = doc.data();
 
-    assert(set)
-    assert(set.name)
+      assert(set);
+      assert(set.name);
 
-    setsByName[set.name] = set
-    cacheSetNames.add(set.name)
-  })
+      setsByName[set.name] = set;
+      cacheSetNames.add(set.name);
+    });
 
-  unchangedSetNames = intersection(cacheSetNames, wisSetNames)
-  addedSetNames = difference(wisSetNames, cacheSetNames)
-  removedSetNames = difference(cacheSetNames, wisSetNames)
+    unchangedSetNames = intersection(cacheSetNames, wisSetNames);
+    addedSetNames = difference(wisSetNames, cacheSetNames);
+    removedSetNames = difference(cacheSetNames, wisSetNames);
 
-  unchangedSets = new Set(Array.from(unchangedSetNames).map(name => setsByName[name]))
-  addedSets = new Set(Array.from(addedSetNames).map(name => setsByName[name]))
-  removedSets = new Set(Array.from(removedSetNames).map(name => setsByName[name]))
+    unchangedSets = new Set(
+      Array.from(unchangedSetNames).map((name) => setsByName[name])
+    );
+    addedSets = new Set(
+      Array.from(addedSetNames).map((name) => setsByName[name])
+    );
+    removedSets = new Set(
+      Array.from(removedSetNames).map((name) => setsByName[name])
+    );
 
-  addedSets.forEach(set => collection.add(set))
-  removedSets.forEach(set => collection.where('name', '==', set.name).get().then(snapshot => snapshot.forEach(doc => doc.ref.delete())))
+    addedSets.forEach((set) => collection.add(set));
+    removedSets.forEach((set) =>
+      collection
+        .where("name", "==", set.name)
+        .get()
+        .then((snapshot) => snapshot.forEach((doc) => doc.ref.delete()))
+    );
 
-  // functions.logger.info(`Unchanged sets: ${Array.from(unchangedSetNames).join(', ')}`)
-  // functions.logger.info(`Removed sets: ${Array.from(removedSetNames).join(', ')}`)
-  // functions.logger.info(`Added sets: ${Array.from(addedSetNames).join(', ')}`)
+    functions.logger.info(
+      `Unchanged sets: ${Array.from(unchangedSetNames).join(", ")}`
+    );
+    functions.logger.info(
+      `Removed sets: ${Array.from(removedSetNames).join(", ")}`
+    );
+    functions.logger.info(
+      `Added sets: ${Array.from(addedSetNames).join(", ")}`
+    );
 
-  if (addedSets.size == 0 && removedSets.size == 0) {
-    functions.logger.info(`No sets changed, not tweeting (${cacheSetNames.size} sets in Firestore, ${wisSetNames.size} sets in WIS API)`)
-    return
-  }
-
-  const config = functions.config()
-  const twitterClient = new twitter.TwitterClient({
-    apiKey: config.twitter.api_key,
-    apiSecret: config.twitter.api_secret,
-    accessToken: config.twitter.access_token,
-    accessTokenSecret: config.twitter.access_token_secret,
-  })
-
-  let tweet = `Standard has changed!\n\n`
-  tweet += `${Array.from(addedSets).map(set => `+ ${set.name} was added`).join("\n")}\n`
-  tweet += `${Array.from(removedSets).map(set => `- ${set.name} was removed`).join("\n")}`
-
-  if (tweet.length > 280) {
-    functions.logger.warn(`Tweet would be too long, trying shorter version: ${tweet}`)
-    tweet = `Standard has changed!\n\n`
-    tweet += `${Array.from(addedSets).map(set => `+ ${set.name}`).join("\n")}\n`
-    tweet += `${Array.from(removedSets).map(set => `- ${set.name}`).join("\n")}`
-
-    if (tweet.length > 280) {
-      functions.logger.error(`Even short tweet would be too long, unsure what to do: ${tweet}`)
-      return
+    if (addedSets.size == 0 && removedSets.size == 0) {
+      functions.logger.info(
+        `No sets changed, not tweeting (${cacheSetNames.size} sets in Firestore, ${wisSetNames.size} sets in WIS API)`
+      );
+      return;
     }
-  }
 
+    const config = functions.config();
+    const twitterClient = new twitter.TwitterClient({
+      apiKey: config.twitter.api_key,
+      apiSecret: config.twitter.api_secret,
+      accessToken: config.twitter.access_token,
+      accessTokenSecret: config.twitter.access_token_secret,
+    });
+    const mastodonClient = await mastodon.login({
+      url: config.mastodon.server_url,
+      accessToken: config.mastodon.access_token,
+    });
 
-  functions.logger.info(`Tweeting: ${tweet}`)
-  twitterResponse = await twitterClient.tweets.statusesUpdate({ status: tweet })
-})
+    let longPost = `Standard has changed!\n\n`;
+    longPost += `${Array.from(addedSets)
+      .map((set) => `+ ${set.name} was added`)
+      .join("\n")}\n`;
+    longPost += `${Array.from(removedSets)
+      .map((set) => `- ${set.name} was removed`)
+      .join("\n")}`;
+
+    let shortPost = `Standard has changed!\n\n`;
+    shortPost += `${Array.from(addedSets)
+      .map((set) => `+ ${set.name}`)
+      .join("\n")}\n`;
+    shortPost += `${Array.from(removedSets)
+      .map((set) => `- ${set.name}`)
+      .join("\n")}`;
+
+    let tweet = longPost;
+
+    // Twittter
+    if (tweet.length > MAX_TWEET_LENGTH) {
+      functions.logger.warn(
+        `Tweet would be too long, trying shorter version: ${tweet}`
+      );
+      tweet = shortPost;
+
+      if (tweet.length > MAX_TWEET_LENGTH) {
+        functions.logger.error(
+          `Even short tweet would be too long, unsure what to do: ${tweet}`
+        );
+        return;
+      }
+    }
+
+    functions.logger.info(`Tweeting: ${tweet}`);
+    twitterResponse = await twitterClient.tweets.statusesUpdate({
+      status: tweet,
+    });
+
+    let toot = longPost;
+
+    // Mastodon
+    if (toot.length > MAX_TOOT_LENGTH) {
+      functions.logger.warn(
+        `Toot would be too long, trying shorter version: ${toot}`
+      );
+      toot = shortPost;
+
+      if (toot.length > MAX_TOOT_LENGTH) {
+        functions.logger.error(
+          `Even short toot would be too long, unsure what to do: ${toot}`
+        );
+        return;
+      }
+    }
+
+    functions.logger.info(`Tooting: ${toot}`);
+    mastodonResponse = await mastodonClient.v1.statuses.create({
+      status: toot,
+      visibility: public,
+    });
+  });
 
 const standardSets = () => {
-  return fetch('https://whatsinstandard.com/api/v6/standard.json').then(response => response.json()).then(body => {
-    return body.sets.filter(set => {
-      return ((Date.parse(set.enterDate.exact) || Infinity) <= Date.now())
-        && ((Date.parse(set.exitDate.exact) || Infinity) > Date.now())
-    })
-  })
-}
+  return fetch("https://whatsinstandard.com/api/v6/standard.json")
+    .then((response) => response.json())
+    .then((body) => {
+      return body.sets.filter((set) => {
+        return (
+          (Date.parse(set.enterDate.exact) || Infinity) <= Date.now() &&
+          (Date.parse(set.exitDate.exact) || Infinity) > Date.now()
+        );
+      });
+    });
+};
 
-const difference = (setA, setB) => new Set([...setA].filter(x => !setB.has(x)))
-const intersection = (setA, setB) => new Set([...setA].filter(x => setB.has(x)))
+const difference = (setA, setB) =>
+  new Set([...setA].filter((x) => !setB.has(x)));
+const intersection = (setA, setB) =>
+  new Set([...setA].filter((x) => setB.has(x)));

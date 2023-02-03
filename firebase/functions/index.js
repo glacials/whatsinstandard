f1139e30bbab61a13e34b98fb6d0ecadd37b2497
glacials <ben@twos.dev>
2023-02-02 19:07:26 -0800

    Genericize tweeter/tooter

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -12,141 +12,126 @@ const db = admin.firestore();
 const MAX_TOOT_LENGTH = 500;
 const MAX_TWEET_LENGTH = 280;
 
-collection = db.collection("twitterbot/last-known/sets");
+twitterCollection = db.collection("twitterbot/last-known/sets");
+mastodonCollection = db.collection("mastodonbot/last-known/sets");
 
 exports.maybeTweet = functions.pubsub
   .schedule("every 5 minutes")
   .onRun(async (context) => {
-    setsByName = {};
+    const config = functions.config();
 
     wisSets = await standardSets();
-    wisSets.forEach((set) => (setsByName[set.name] = set));
-    wisSetNames = new Set(wisSets.map((set) => set.name));
 
-    cacheSets = await collection.get();
-    cacheSetNames = new Set();
-    cacheSets.forEach((doc) => {
-      const set = doc.data();
-
-      assert(set);
-      assert(set.name);
-
-      setsByName[set.name] = set;
-      cacheSetNames.add(set.name);
-    });
-
-    unchangedSetNames = intersection(cacheSetNames, wisSetNames);
-    addedSetNames = difference(wisSetNames, cacheSetNames);
-    removedSetNames = difference(cacheSetNames, wisSetNames);
-
-    unchangedSets = new Set(
-      Array.from(unchangedSetNames).map((name) => setsByName[name])
-    );
-    addedSets = new Set(
-      Array.from(addedSetNames).map((name) => setsByName[name])
-    );
-    removedSets = new Set(
-      Array.from(removedSetNames).map((name) => setsByName[name])
-    );
-
-    addedSets.forEach((set) => collection.add(set));
-    removedSets.forEach((set) =>
-      collection
-        .where("name", "==", set.name)
-        .get()
-        .then((snapshot) => snapshot.forEach((doc) => doc.ref.delete()))
-    );
-
-    functions.logger.info(
-      `Unchanged sets: ${Array.from(unchangedSetNames).join(", ")}`
-    );
-    functions.logger.info(
-      `Removed sets: ${Array.from(removedSetNames).join(", ")}`
-    );
-    functions.logger.info(
-      `Added sets: ${Array.from(addedSetNames).join(", ")}`
-    );
-
-    if (addedSets.size == 0 && removedSets.size == 0) {
-      functions.logger.info(
-        `No sets changed, not tweeting (${cacheSetNames.size} sets in Firestore, ${wisSetNames.size} sets in WIS API)`
-      );
-      return;
-    }
-
-    const config = functions.config();
-    const twitterClient = new twitter.TwitterClient({
-      apiKey: config.twitter.api_key,
-      apiSecret: config.twitter.api_secret,
-      accessToken: config.twitter.access_token,
-      accessTokenSecret: config.twitter.access_token_secret,
-    });
-    const mastodonClient = await mastodon.login({
-      url: config.mastodon.server_url,
-      accessToken: config.mastodon.access_token,
-    });
-
-    let longPost = `Standard has changed!\n\n`;
-    longPost += `${Array.from(addedSets)
-      .map((set) => `+ ${set.name} was added`)
-      .join("\n")}\n`;
-    longPost += `${Array.from(removedSets)
-      .map((set) => `- ${set.name} was removed`)
-      .join("\n")}`;
-
-    let shortPost = `Standard has changed!\n\n`;
-    shortPost += `${Array.from(addedSets)
-      .map((set) => `+ ${set.name}`)
-      .join("\n")}\n`;
-    shortPost += `${Array.from(removedSets)
-      .map((set) => `- ${set.name}`)
-      .join("\n")}`;
-
-    let tweet = longPost;
-
-    // Twittter
-    if (tweet.length > MAX_TWEET_LENGTH) {
-      functions.logger.warn(
-        `Tweet would be too long, trying shorter version: ${tweet}`
-      );
-      tweet = shortPost;
-
-      if (tweet.length > MAX_TWEET_LENGTH) {
-        functions.logger.error(
-          `Even short tweet would be too long, unsure what to do: ${tweet}`
+    await shouldPost(twitterCollection, wisSets).then(async (data) => {
+      if (data.addedSets.size == 0 && data.removedSets.size == 0) {
+        functions.logger.info(
+          `No sets changed, not tweeting (${cacheSetNames.size} sets in Firestore, ${wisSetNames.size} sets in WIS API)`
         );
         return;
       }
-    }
 
-    functions.logger.info(`Tweeting: ${tweet}`);
-    twitterResponse = await twitterClient.tweets.statusesUpdate({
-      status: tweet,
+      const twitterClient = new twitter.TwitterClient({
+        apiKey: config.twitter.api_key,
+        apiSecret: config.twitter.api_secret,
+        accessToken: config.twitter.access_token,
+        accessTokenSecret: config.twitter.access_token_secret,
+      });
+
+      const tweet = craftPost(data, MAX_TWEET_LENGTH);
+      if (tweet === null) {
+        return;
+      }
+
+      functions.logger.info(`Tweeting: ${tweet}`);
+      response = await twitterClient.tweets.statusesUpdate({
+        status: tweet,
+      });
+      functions.logger.info(`Twitter response: ${response}`);
     });
 
-    let toot = longPost;
-
-    // Mastodon
-    if (toot.length > MAX_TOOT_LENGTH) {
-      functions.logger.warn(
-        `Toot would be too long, trying shorter version: ${toot}`
-      );
-      toot = shortPost;
-
-      if (toot.length > MAX_TOOT_LENGTH) {
-        functions.logger.error(
-          `Even short toot would be too long, unsure what to do: ${toot}`
+    await shouldPost(mastodonCollection, wisSets).then(async (data) => {
+      if (data.addedSets.size == 0 && data.removedSets.size == 0) {
+        functions.logger.info(
+          `No sets changed, not tooting (${cacheSetNames.size} sets in Firestore, ${wisSetNames.size} sets in WIS API)`
         );
         return;
       }
-    }
 
-    functions.logger.info(`Tooting: ${toot}`);
-    mastodonResponse = await mastodonClient.v1.statuses.create({
-      status: toot,
-      visibility: public,
+      const mastodonClient = await mastodon.login({
+        url: config.mastodon.server_url,
+        accessToken: config.mastodon.access_token,
+      });
+
+      const toot = craftPost(data, MAX_TOOT_LENGTH);
+      if (toot === null) {
+        return;
+      }
+
+      functions.logger.info(`Tooting: ${toot}`);
+      response = await mastodonClient.v1.statuses.create({
+        status: toot,
+        visibility: "public",
+      });
+      functions.logger.info(`Mastodon response: ${response}`);
     });
   });
+
+// Uses the given collection to see if we need to post about the current set
+// state. This can differ if, for example, one platform failed to post in an
+// earlier invocation but one succeeded.
+//
+// WARNING: When you spin up a new platform its collection will be empty, so a
+// post will be made on the next invocation even if there was not a rotation
+// recently.
+const shouldPost = async (collection, wisSets) => {
+  setsByName = {};
+  wisSets.forEach((set) => (setsByName[set.name] = set));
+  wisSetNames = new Set(wisSets.map((set) => set.name));
+
+  cacheSets = await collection.get();
+  cacheSetNames = new Set();
+  cacheSets.forEach((doc) => {
+    const set = doc.data();
+
+    assert(set);
+    assert(set.name);
+
+    setsByName[set.name] = set;
+    cacheSetNames.add(set.name);
+  });
+
+  unchangedSetNames = intersection(cacheSetNames, wisSetNames);
+  addedSetNames = difference(wisSetNames, cacheSetNames);
+  removedSetNames = difference(cacheSetNames, wisSetNames);
+
+  unchangedSets = new Set(
+    Array.from(unchangedSetNames).map((name) => setsByName[name])
+  );
+  addedSets = new Set(
+    Array.from(addedSetNames).map((name) => setsByName[name])
+  );
+  removedSets = new Set(
+    Array.from(removedSetNames).map((name) => setsByName[name])
+  );
+
+  addedSets.forEach((set) => collection.add(set));
+  removedSets.forEach((set) =>
+    collection
+      .where("name", "==", set.name)
+      .get()
+      .then((snapshot) => snapshot.forEach((doc) => doc.ref.delete()))
+  );
+
+  functions.logger.info(
+    `Unchanged sets: ${Array.from(unchangedSetNames).join(", ")}`
+  );
+  functions.logger.info(
+    `Removed sets: ${Array.from(removedSetNames).join(", ")}`
+  );
+  functions.logger.info(`Added sets: ${Array.from(addedSetNames).join(", ")}`);
+
+  return { addedSets: addedSets, removedSets: removedSets };
+};
 
 const standardSets = () => {
   return fetch("https://whatsinstandard.com/api/v6/standard.json")
@@ -159,6 +144,41 @@ const standardSets = () => {
         );
       });
     });
+};
+
+// Data should look like {addedSets: [...], removedSets: [...]}
+const craftPost = (data, characterLimit) => {
+  let longPost = `Standard has changed!\n\n`;
+  longPost += `${Array.from(data.addedSets)
+    .map((set) => `+ ${set.name} was added`)
+    .join("\n")}\n`;
+  longPost += `${Array.from(data.removedSets)
+    .map((set) => `- ${set.name} was removed`)
+    .join("\n")}`;
+
+  let shortPost = `Standard has changed!\n\n`;
+  shortPost += `${Array.from(data.addedSets)
+    .map((set) => `+ ${set.name}`)
+    .join("\n")}\n`;
+  shortPost += `${Array.from(data.removedSets)
+    .map((set) => `- ${set.name}`)
+    .join("\n")}`;
+
+  if (longPost.length < characterLimit) {
+    return longPost;
+  }
+  functions.logger.warn(
+    `Post would be too long, trying shorter version. Long post was: ${longPost}`
+  );
+
+  if (shortPost.length < characterLimit) {
+    return shortPost;
+  }
+  functions.logger.error(
+    `Even short post would be too long, unsure what to do. Short post was: ${shortPost}`
+  );
+
+  return null;
 };
 
 const difference = (setA, setB) =>
